b44cb0e2ae33049444c0f1a1fd84cf1f6aadd57a
Klimov-V <klimov_vitaliy@icloud.com>
2020-06-06 10:47:36 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+(() => {
+    'use strict'
+
+    
+
+    // function getData() {
+    //     let data = localStorage.getItem('tasks');
+    //     return data = data ? JSON.parse(data) : [];
+    // }
+
+    // function setData(data) {
+    //     data = JSON.stringify(data);
+    //     localStorage.setItem('tasks', data);
+    // }
+ 
+})();
